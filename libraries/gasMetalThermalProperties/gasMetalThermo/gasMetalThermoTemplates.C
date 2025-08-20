de9d72757259b5e8881c0492f0eaaa599a2e1726
Mygetsy <mygetsy16@yandex.ru>
2025-08-20 12:26:00 +0300

    [slmMeltPoolFoam] rewrite energy equation
    1. The hEq.h was changed to be simmilar notation to paper
    2. Change passive fields calculation heat conduction

--- libraries/gasMetalThermalProperties/gasMetalThermo/gasMetalThermoTemplates.C
+++ libraries/gasMetalThermalProperties/gasMetalThermo/gasMetalThermoTemplates.C
@@ -30,30 +30,6 @@ License
 
 // * * * * * * * * * * * * * * * Local Functions * * * * * * * * * * * * * * //
 
-namespace
-{
-
-Foam::scalar gasMetalAverage
-(
-    const Foam::Polynomial<2>& gasProperty,
-    const Foam::Polynomial<2>& liquidProperty,
-    const Foam::Polynomial<2>& solidProperty,
-    Foam::scalar T,
-    Foam::scalar liquidFraction,
-    Foam::scalar gasFraction
-)
-{
-    Foam::scalar solidFraction = 1 - liquidFraction - gasFraction;
-
-    return
-        solidProperty.value(T)*solidFraction
-      + liquidProperty.value(T)*liquidFraction
-      + gasProperty.value(T)*gasFraction;
-}
-
-
-} // End namespace
-
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
 
 template<class T1, class T2, class T3>
@@ -78,7 +54,7 @@ Foam::tmp<T1> Foam::gasMetalThermo::Cp
         {
             scalar alphaM = 1 - alphaG;
             scalar rho = alphaG*rhoGas_ + phi*rhoLiquid_*(1 + betaLiquid_*(T - Tmelting_)) + (alphaM - phi)*rhoSolid_;
-            
+
             return (alphaG*rhoGas_*gas_.Cp.value(T)
                     + (alphaM - phi)*rhoSolid_*solid_.Cp.value(T)
                     + phi*rhoLiquid_*(1 + betaLiquid_*(T - Tmelting_))*liquid_.Cp.value(T))/rho;
@@ -131,9 +107,9 @@ Foam::tmp<T3> Foam::gasMetalThermo::h
         {
             scalar alphaM = 1 - alphaG;
             scalar rho = alphaG*rhoGas_ + phi*rhoLiquid_*(1 + betaLiquid_*(T - Tmelting_)) + (alphaM - phi)*rhoSolid_;
-            
+
             return (alphaG*rhoGas_*gas_.Cp.integral(0, T)
-                    + (alphaM - phi)*rhoSolid_*solid_.Cp.integral(0, T) 
+                    + (alphaM - phi)*rhoSolid_*solid_.Cp.integral(0, T)
                     + phi*rhoLiquid_*(1 + betaLiquid_*(T - Tmelting_))*(
                     solid_.Cp.integral(0, Tmelting_) + liquid_.Cp.integral(Tmelting_, T) + Hfusion_)
                     )/rho;
@@ -253,7 +229,7 @@ Foam::tmp<T1> Foam::gasMetalThermo::T
     const T2& liquidFraction,
     const T3& gasFraction
 ) const
-{   
+{
     return generateGeometricField<T1>
     (
         "T",
@@ -265,21 +241,21 @@ Foam::tmp<T1> Foam::gasMetalThermo::T
             scalar alphaM = 1 - alphaG;
             scalar hLiquidTmelt = solid_.Cp.integral(0, Tmelting_) + Hfusion_;
 
-            scalar A = (alphaM - phi)*rhoSolid_*solid_.Cp.derivative(0) 
+            scalar A = (alphaM - phi)*rhoSolid_*solid_.Cp.derivative(0)
                         + 2*betaLiquid_*phi*rhoLiquid_*liquid_.Cp.value(0);
 
             scalar B = betaLiquid_*phi*rhoLiquid_*(hLiquidTmelt - h)
                         + (1 - 2*Tmelting_*betaLiquid_)*phi*rhoLiquid_*liquid_.Cp.value(0)
                         + (alphaM - phi)*rhoSolid_*solid_.Cp.value(0)
                         + alphaG*rhoGas_*gas_.Cp.value(0);
-            
+
             scalar C = hLiquidTmelt*phi*rhoLiquid_*(Tmelting_*betaLiquid_ - 1)
                         + Tmelting_*phi*rhoLiquid_*liquid_.Cp.value(0)*(1 - Tmelting_*betaLiquid_)
                         + (
                             (1 - Tmelting_*betaLiquid_)*phi*rhoLiquid_ + (alphaM - phi)*rhoSolid_ + alphaG*rhoGas_
                         )*h;
 
-            return 
+            return
             mag(A) > ROOTSMALL
               ? (Foam::sqrt(sqr(B) + 2*A*C) - B)/A
               : C/B;
@@ -294,7 +270,7 @@ Foam::tmp<T2> Foam::gasMetalThermo::phiCalc
     const T1& h,
     const T2& gasFraction
 ) const
-{   
+{
     return generateGeometricField<T2>
     (
         "phiCalc",
@@ -312,10 +288,10 @@ Foam::tmp<T2> Foam::gasMetalThermo::phiCalc
 
             scalar gasLiquidEnthalpy = (alphaG*rhoGas_*hGasTmelt + alphaM*rhoLiquid_*hLiquidTmelt)
                                     /(alphaG*rhoGas_ + alphaM*rhoLiquid_);
-            
+
             scalar phiTmelt = (h*(alphaG*rhoGas_ + alphaM*rhoSolid_) - alphaG*rhoGas_*hGasTmelt - alphaM*rhoSolid_*hSolidTmelt)
                             /(h*(rhoSolid_ - rhoLiquid_) + rhoLiquid_*hLiquidTmelt - rhoSolid_*hSolidTmelt);
-            
+
 
             if (h < gasSolidEnthalpy){
                 return 0.;
